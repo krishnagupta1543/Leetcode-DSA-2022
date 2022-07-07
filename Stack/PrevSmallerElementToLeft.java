fa75dd23f28e5c4507f03864d8dd9aa8a05f8c7b
krishna <kg18960@gmail.com>
2022-07-07 15:00:10 +0530

    PrevSmallerElementToLeft.java

---
+++ Stack/PrevSmallerElementToLeft.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Stack;
+public class PrevSmallerElementToLeft {
+    public static void main(String[] args) {
+        int[] arr = {11, 13,21,3};
+        int[] ans = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (stack.isEmpty()!=true && arr[stack.peek()]>arr[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans[i] = -1;
+            }else{
+                ans[i] = stack.peek();
+            }
+            stack.push(i);
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
+
