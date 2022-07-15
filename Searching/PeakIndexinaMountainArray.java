9fe634148d7c3c5f2ca8e2c85a07c5c5a56e8d18
krishna <kg18960@gmail.com>
2022-07-15 18:24:48 +0530

    added

---
+++ Searching/PeakIndexinaMountainArray.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+     int start = 0;
+        int end = arr.length - 1;
+        while(start < end){
+            int mid = (start+end)/2;
+            if(arr[mid]>arr[mid+1]){
+                end = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
