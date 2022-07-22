51fd3cfa8ddc83a6054f296a09ee0e5a5a49cc11
krishna <kg18960@gmail.com>
2022-07-22 15:40:02 +0530

    SearchInTree modified

--- Tree/SearchInTree.java
+++ Tree/SearchInTree.java
@@ -1,3 +1,4 @@
+// RECURSIVE
 public static boolean Search(Node node, int target){
         if(node.data == target){
             return true;
@@ -5,4 +6,20 @@ public static boolean Search(Node node, int target){
             return false;
         }
         return Search(node.left, target) || Search(node.right, target);
+    }
+// ITERATIVE
+public static boolean Search2(Node node, int target){
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        while(!queue.isEmpty()){
+            Node temp = queue.poll();
+            if(temp.data == target){
+                return true;
+            }else if(temp.left!=null){
+                queue.add(temp.left);
+            }else if(temp.right!=null){
+                queue.add(temp.right);
+            }
+        }
+        return false;
     }
