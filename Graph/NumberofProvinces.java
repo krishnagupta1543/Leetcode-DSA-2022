9595d1f079cc74ab27a4fced29f3cfb13be80513
krishna gupta <kg18960@gmail.com>
2023-04-24 22:36:32 +0530

    number of provinces added

---
+++ Graph/NumberofProvinces.java
@@ -0,0 +1,35 @@
+// https://practice.geeksforgeeks.org/problems/number-of-provinces/1
+class Solution {
+    public static void dfs(int vertice, int V, ArrayList<ArrayList<Integer>> adjLst,int[]visited){
+        if(visited[vertice] == 1){
+            return;
+        }
+        visited[vertice] = 1;
+        for(int i = 0; i < adjLst.get(vertice).size(); i++){
+            dfs(adjLst.get(vertice).get(i), V, adjLst, visited);
+        }
+    }
+    static int numProvinces(ArrayList<ArrayList<Integer>> arr, int V) {
+        // code here
+         ArrayList<ArrayList<Integer>> adjLst = new ArrayList<>();
+        for(int i = 0; i < V; i++){
+            adjLst.add(new ArrayList<>());
+        }
+        for(int i = 0; i < V;i++){
+            for(int j = 0; j < V; j++){
+                if(arr.get(i).get(j) == 1 && i != j){
+                    adjLst.get(i).add(j);
+                }
+            }
+        }
+        int province = 0;
+        int[] visited = new int[V];
+        for (int i = 0; i < V; i++) {
+            if(visited[i] != 1){
+                province+=1;
+                dfs(i, V, adjLst, visited);
+            }
+        }
+        return province;
+    }
+};
